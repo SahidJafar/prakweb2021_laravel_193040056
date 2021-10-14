a9bb2aebb5484555638d9f1790ca32a60a9b631c
SahidJafar <sahid11.sj@gmail.com>
2021-10-14 23:55:31 +0700

    Post Category dan Eloquent Relationship

--- coba-laravel/resources/views/post.blade.php
+++ coba-laravel/resources/views/post.blade.php
@@ -3,6 +3,9 @@
 @section('container')
 <article>
 <h2>{{ $post->title }}</h2>
+
+<p>By. Sahid Jafar in <a href="/categories/{{$post->category->slug }}">{{ $post->category->name  }}</a></p>
+
 <h5>{!! $post->body !!}</h5>
 </article>
 
