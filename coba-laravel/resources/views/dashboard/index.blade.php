2a72dffdcef2889418aaed3822120cd8c6ac0aef
SahidJafar <sahid11.sj@gmail.com>
2021-10-30 06:54:55 +0700

    User Login & Middleware

---
+++ coba-laravel/resources/views/dashboard/index.blade.php
@@ -0,0 +1,1 @@
+<h1>Welcome,Sahid Jafar</h1>
--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -53,12 +54,17 @@ Route::get('/categories', function(){
 });
 
 // middleware('guest') digunakan untuk user yang belum terauthenrifikasi
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
+
 
 // Register
-Route::get('/register', [RegisterController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
 
+// login dashboard
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
 
 // Route::get('/categories/{category:slug}',function(Category $category){
 //     return view('posts', [
